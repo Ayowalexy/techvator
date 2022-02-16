8dd758d80840e5f7de350239e60d69e5e02b294b
knowsarzehmeh <osarzeh@gmail.com>
2022-02-16 23:23:31 +0100

    Hero Section Completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Heading, Stack, Text } from '@chakra-ui/react'
 import Head from 'next/head'
+import Hero from '../components/Home/Hero'
 import Layout from '../components/Layout'
 
 export default function Home():JSX.Element {
@@ -12,19 +13,7 @@ export default function Home():JSX.Element {
       </Head>
 
       <Layout>
-      <Stack>
-        <Heading>The spectacle before us was indeed sublime.</Heading>
-        <Text>
-          Apparently we had reached a great height in the atmosphere, for the
-          sky was a dead black, and the stars had ceased to twinkle. By the same
-          illusion which lifts the horizon of the sea to the level of the
-          spectator on a hillside, the sable cloud beneath was dished out, and
-          the car seemed to float in the middle of an immense dark sphere, whose
-          upper half was strewn with silver. Looking down into the dark gulf
-          below, I could see a ruddy light streaming through a rift in the
-          clouds.
-        </Text>
-      </Stack>
+          <Hero />
       </Layout>
     </div>
   )

d9d77f619ee6f6d323db9450caeb0739f0a34119
knowsarzehmeh <osarzeh@gmail.com>
2022-02-19 01:27:52 +0100

    amahulbist lifestyle

---
+++ components/HeaderTag.tsx
@@ -0,0 +1,17 @@
+import { Heading, HeadingProps, useTheme } from '@chakra-ui/react'
+import { type } from 'os'
+import React from 'react'
+
+
+
+function HeaderTag({ children, ...rest}: HeadingProps): JSX.Element {
+     const theme = useTheme()
+     const {white} = theme.colors.brand
+  return (
+    <Heading as='h3' my='3rem' color={white} fontSize='3.1rem' letterSpacing='.2rem'  fontWeight='500' textAlign='center' {...rest}> 
+         {children}
+    </Heading>
+  )
+}
+
+export default HeaderTag
--- data/the-amahlubist.ts
+++ data/the-amahlubist.ts
@@ -21,3 +21,26 @@ export const theAmahlubist = [
         name: 'Hlubi Nboya'
     },
 ]
+
+export const amahlubistLifeStyle = [
+    {
+        title: 'Success-Survey',
+        subTitle: 'This will house alot of questions for our website visitors to answer, to find their path, to claiming their rightful place at the top',
+        url: '#'
+    },
+    {
+        title: 'Power-Pioneering-Pledge',
+        subTitle: 'This will house personal pledges of the commitment to create legacies',
+        url: '#'
+    },
+    {
+        title: 'Owning \n Financial-Freedom',
+        subTitle: 'This will house everything "Black-Excellent" ideas shared from a personal perspective from financial gurus like Manzini Zungu, Zizipho Zungu, George Dube, and the likes of Patrice Motsipe etc. ',
+        url: '#'
+    },
+    {
+        title: 'Amahlubi Swagness',
+        subTitle: 'This will house everything "Black-Excellent" ideas shared from a personal perspective on fashion, hair, attire, drip-dress- advice on owning & solidifying your personal individual look...sponsored by ZunguNation...',
+        url: '#'
+    },
+]

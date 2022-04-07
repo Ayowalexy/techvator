8a02e029f7dc2b3c7cda6ad022c7cf1742cec1aa
knowsarzehmeh <osarzeh@gmail.com>
2022-04-07 11:25:16 +0100

    Bug Fixes

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,18 +1,18 @@
-import { Box, BoxProps } from '@chakra-ui/react'
-import { useTheme } from '@emotion/react'
-import React from 'react'
+import { Box, BoxProps, useTheme } from "@chakra-ui/react";
 
-type ContainerProps = BoxProps 
+import React from "react";
 
-function Container({ children, ...rest}: ContainerProps): JSX.Element {
- const theme =  useTheme()
- const {black} = theme.colors.brand
+type ContainerProps = BoxProps;
+
+function Container({ children, ...rest }: ContainerProps): JSX.Element {
+  const theme = useTheme();
+  const { black } = theme.colors.brand;
 
   return (
-    <Box bgColor={black} px='2.4rem' {...rest}>
-        {children}
+    <Box bgColor={black} px="2.4rem" {...rest}>
+      {children}
     </Box>
-  )
+  );
 }
 
-export default Container
+export default Container;
